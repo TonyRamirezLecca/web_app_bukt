fbdf20ff6d5467885885241219666e057d56759c
Tony Ramirez <33140550+TonyRamirezLecca@users.noreply.github.com>
2020-09-19 22:51:25 -0600

    Add files via upload

---
+++ precache-manifest.af8d4ba9722d9d4011f38f954f4b7761.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec603d6642afb0a362fe8a41c4ab7782",
+    "url": "/index.html"
+  },
+  {
+    "revision": "144b5b0ca7e86e3aacca",
+    "url": "/static/css/main.22711d01.chunk.css"
+  },
+  {
+    "revision": "7e2a7533ba02953cfb6d",
+    "url": "/static/js/2.6d90e3ed.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6d90e3ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "144b5b0ca7e86e3aacca",
+    "url": "/static/js/main.343ddcb3.chunk.js"
+  },
+  {
+    "revision": "bbc78a50a9543eeb32ba",
+    "url": "/static/js/runtime-main.cd749d43.js"
+  },
+  {
+    "revision": "3618bc677078225773b5411305037d89",
+    "url": "/static/media/girl-in-plane.3618bc67.jpg"
+  },
+  {
+    "revision": "9a4a8c9ff04bc9a31ad3aba4d547d66d",
+    "url": "/static/media/red-bucket-icon.9a4a8c9f.jpg"
+  },
+  {
+    "revision": "02d629480c0aa6e46e743d3b51b84ff3",
+    "url": "/static/media/sunset-hiker.02d62948.jpg"
+  }
+]);
